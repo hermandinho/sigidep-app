2062c8fcb0505324d8e5793a648bcf10b026e6cb
Herman Demsong <hdemsongtsamo@gmail.com>
2021-10-12 11:29:32 +0100

    setup swagger ui

--- packages/sigidep-api/src/main.ts
+++ packages/sigidep-api/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  const document = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder()
+      .setTitle('SIGIDEP API')
+      .setDescription('SIGIDEP APIs description')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .addBearerAuth()
+      .build(),
+  );
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.API_PORT || 3000);
 }
 bootstrap();
