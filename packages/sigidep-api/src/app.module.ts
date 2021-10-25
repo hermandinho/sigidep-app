3953b5559568352bf2f2fabf04193ad7361b2478
Herman Demsong <hdemsongtsamo@gmail.com>
2021-10-25 16:47:06 +0100

    Heroku setup

--- packages/sigidep-api/src/app.module.ts
+++ packages/sigidep-api/src/app.module.ts
@@ -26,7 +26,7 @@ import { StructureModule } from './modules/structure/structure.module';
           // subscribers: ['dist/subscribers/*.subscriber.js'],
           synchronize: true,
           type: 'postgres',
-          port: +config.get('DB_PORT'),
+          port: +process.env.DB_PORT,
           // dropSchema: true,
           // logging: true,
           // migrationsTableName: 'migrations',
