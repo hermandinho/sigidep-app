91dfda4894c5991345934dedb4d95d46a55b7567
Herman Demsong <hdemsongtsamo@gmail.com>
2021-11-05 19:57:27 +0100

    fix api url

--- packages/sigidep-client/src/environments/environment.prod.ts
+++ packages/sigidep-client/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'https://sigidep-api-dev.herokuapp.com',
+  // API_URL: 'https://sigidep-api-dev.herokuapp.com',
+  API_URL: 'http://141.94.77.141:3030',
 };
