b110810a5e0e2bd55ebb3c18b2d5108f67f79462
tsognong <44397824+tsognong@users.noreply.github.com>
2022-01-23 08:59:41 +0100

    start carnet mandat CRUD

---
+++ packages/sigidep-api/src/entities/carnet-mandat.entity.ts
@@ -0,0 +1,63 @@
+import { Column, Entity, JoinColumn, ManyToOne, Unique } from 'typeorm';
+import { AgentEntity, BaseEntity } from '.';
+
+@Entity({
+  name: 'carnets_mandats',
+  orderBy: {
+    code: 'ASC',
+  },
+})
+@Unique('UQ_CARNET_MANDAT_CODE', ['code'])
+export class CarnetMandatEntity extends BaseEntity {
+  @Column({ name: 'code', nullable: false })
+  public code: string;
+
+  @Column({ name: 'premier_feuillet', nullable: true })
+  public premierFeuillet: string;
+
+  @Column({ name: 'dernier_feuillet', nullable: true })
+  public dernierFeuillet: string;
+
+  @Column({ name: 'date_enreg', nullable: true, type: 'date' })
+  public dateEnreg: Date;
+
+  @ManyToOne(() => AgentEntity, (object) => object.id, {
+    cascade: true,
+    eager: false,
+  })
+  @JoinColumn({ name: 'gestionnaire' })
+  /** TO BE CHANGED TO Fournisseur entity later, at least a fournisseur is an agent */
+  public gestionnaire: AgentEntity;
+
+  @Column({ name: 'date_affectation', nullable: true, type: 'date' })
+  public dateAffectation: Date;
+
+  @Column({ name: 'date_retrait', nullable: true, type: 'date' })
+  public dateRetrait: Date;
+
+  @Column({ name: 'mat_agent_retrait', nullable: true })
+  public matAgentRetrait: string;
+
+  @Column({ name: 'nom_agent_retrait', nullable: true })
+  public nomAgentRetrait: string;
+
+  @Column({ name: 'num_cni_agent_retrait', nullable: true })
+  public numCNIAgentRetrait: string;
+
+  @Column({ name: 'date_delivrance_cni', nullable: true, type: 'date' })
+  public dateDelivranceCNI: Date;
+
+  @Column({ name: 'lieu_delivrance_cni', nullable: true })
+  public lieuDelivranceCNI: string;
+
+  /** TO ADD LATER WHEN MANDAT WILL BE AVAILABLE 
+  @OneToMany(() => Mandat, (mandat) => mandat.carnet)
+  public mandats: Mandat[];
+  */
+  constructor(param?: Partial<CarnetMandatEntity>) {
+    super();
+    if (param) {
+      Object.assign(this, param);
+    }
+  }
+}
