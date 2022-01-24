5d9e4b18d6fb0941621eecaadc2a169fd214e4a8
Kuete Donald <kuetedonald@gmail.com>
2022-01-24 15:45:23 +0100

    minor bug fixes

--- packages/sigidep-api/src/modules/carnets-mandats/carnet-mandat.service.ts
+++ packages/sigidep-api/src/modules/carnets-mandats/carnet-mandat.service.ts
@@ -8,7 +8,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserEntity } from '@entities/user.entity';
 import { CarnetMandatEntity } from '@entities/carnet-mandat.entity';
-import { EditCarnetMandatDTO } from '@modules/carnets-mandats/dto/edit-carnet-mandat.dto';
+import { EditCarnetMandatDTO } from './dto/edit-carnet-mandat.dto';
 
 @Injectable()
 export class CarnetMandatService {
