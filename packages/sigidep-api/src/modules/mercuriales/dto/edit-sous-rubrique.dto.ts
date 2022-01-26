e090c976a3a08830208de2c5a407d62ff38ff711
tsognong <44397824+tsognong@users.noreply.github.com>
2022-01-26 09:00:37 +0100

    remove circular dependency

--- packages/sigidep-api/src/modules/mercuriales/dto/edit-sous-rubrique.dto.ts
+++ packages/sigidep-api/src/modules/mercuriales/dto/edit-sous-rubrique.dto.ts
@@ -1,6 +1,6 @@
 import { IsNotEmpty, Length } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
-import { EditArticleMercurialeDTO } from '../dto';
+import { EditRubriqueDTO } from './edit-rubrique.dto';
 
 export class EditSousRubriqueDTO {
   public id?: number;
@@ -10,9 +10,9 @@ export class EditSousRubriqueDTO {
   @Length(3, 3)
   public code: string;
 
-  @ApiProperty({ example: 'MEUBLES EN CUIR', required: true })
+  @ApiProperty({ example: 'MEUBLES EN CUIR', required: false })
   public label?: string;
 
-  @ApiProperty({ required: false })
-  public articles: EditArticleMercurialeDTO[];
+  @ApiProperty({ example: '', required: true })
+  public rubrique: EditRubriqueDTO;
 }
