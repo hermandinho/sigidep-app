3327d32fa567e40d0ed8dd136494889b38afd08e
TSOGNONG FIDELE <tsognong.fidele@gmail.com>
2022-03-03 18:33:55 +0100

    traduction

--- packages/sigidep-client/src/app/layout/layout-routing.module.ts
+++ packages/sigidep-client/src/app/layout/layout-routing.module.ts
@@ -28,6 +28,8 @@ import { TypesProceduresComponent } from '@pages/types-procedures/types-procedur
 import { PiecesJointesComponent } from '@pages/pieces-jointes/pieces-jointes.component';
 import { EncoursComponent } from '@pages/encours/encours.component';
 import { EngagementsComponent } from '@pages/engagements/engagements.component';
+import { ProceduresComponent } from '@pages/procedures/procedures.component';
+import { TaxesComponent } from '@pages/taxes/taxes.component';
 
 const routes: Routes = [
   {
@@ -255,6 +257,7 @@ const routes: Routes = [
       },
       {
         path: 'taxes',
+        component: TaxesComponent,
         canActivate: [AuthGuard],
         data: {
           permissions: [...MenuPermissions.taxes],
@@ -264,6 +267,7 @@ const routes: Routes = [
       },
       {
         path: 'procedures',
+        component: ProceduresComponent,
         canActivate: [AuthGuard],
         data: {
           permissions: [...MenuPermissions.procedures],
