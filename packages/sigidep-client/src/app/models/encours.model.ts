4271314f2468f6144afaa2db7b2634794e551b2e
Yvan2Ero <yvankanaj@gmail.com>
2022-03-16 17:55:41 +0100

    Retouche de la structure de encours

--- packages/sigidep-client/src/app/models/encours.model.ts
+++ packages/sigidep-client/src/app/models/encours.model.ts
@@ -14,67 +14,36 @@ import { BaseModel } from './base.model';
 import { PhysicalUnitModel } from './physical-unit.entity';
 
 export class EncoursModel extends BaseModel {
-  public exerciseCode!: string;
-  public subProgram!: string;
-  public active!: string;
-  public task!: string;
-  public administrativeUnit!: string;
-  public imputation!: string;
-  public operationLabelFr!: string;
-  public operationLabelEn!: string;
-  public livrableFr!: string;
-  public sourceVerificationFr!: string;
-  public gestionnaire!: string;
-  public modeGestion!: string;
-  public cpInitial!: number;
-  public aeInitial!: number;
-  public sourceFinencement!: string;
-  public region!: string;
-  public department!: string;
-  public arrondissement!: string;
-  public locality!: string;
-  public codeUnitePhysique!: string;
-  public labelUnitePhysique!: string;
-  public qteUnitePhysique!: number;
-  public puUnitePhysique!: string;
-  public amountUnitePhysique!: string;
-  public operation!: string;
-  public action!: string;
+  exerciseCode!: string;
+  subProgram!: string;
+  active!: string;
+  task!: string;
+  administrativeUnit!: string;
+  imputation!: string;
+  operationLabelFr!: string;
+  operationLabelEn!: string;
+  livrableFr!: string;
+  sourceVerificationFr!: string;
+  gestionnaire!: string;
+  modeGestion!: string;
+  cpInitial!: number;
+  aeInitial!: number;
+  sourceFinencement!: string;
+  region!: string;
+  department!: string;
+  arrondissement!: string;
+  locality!: string;
+  codeUnitePhysique!: string;
+  labelUnitePhysique!: string;
+  qteUnitePhysique!: number;
+  puUnitePhysique!: string;
+  amountUnitePhysique!: string;
+  operation!: string;
+  action!: string;
+  operations!: SubProgramActivityTaskOperationModel[];
 
 
 //------------------------------------------------------
-  exercise!: ExerciseModel;
-  sousProgramme!: SubProgramModel;
-  activities!: SubProgramActivityModel[];
-  tasks!: SubProgramActivityTaskModel[];
-  operations!: SubProgramActivityTaskOperationModel[];
-  imputations!: string[];
-  adminUnits!: AdministrativeUnitModel[];
-  livrables!: string[];
-  sourceVerif!: string[];
-  modeGestions!: string[];
-  gestionnaires!: string[];
-  regions!: RegionsModel[];
-  departments!: DepartmentModel[];
-  arrondissements!: ArrondissementModel[];
-  localities!: string[];
-  unitePhysiques!: PhysicalUnitModel[];
-  libelleUnitePhys!: string[];
-  quantiteUnitePhys!: number[];
-  puUnitePhys!: number[];
-  montantUnitePhys!: number[];
-  aeInit!: number[];
-  cpInit!: number[];
-  cpInitRevisee!: number[];
-  aeInitRevisee!: number[];
-  aeDisponible!: number[];
-  cpDisponible!: number[];
-  aeEngagJuridique!: number[];
-  cpMandat!: number[];
-  aeDispoANouveau!: number[];
-  cpDispoANouveau!: number[];
-  valeurSeuil!: number;
-  activity!: string;
 
   /***
    * Statistiques mise en ligne
@@ -97,11 +66,6 @@ export class EncoursModel extends BaseModel {
     super();
     if (params) {
       Object.assign(this, params);
-      this.nombreActivites = params.activities?.length||0
-      this.nombreTasks = params.tasks?.length||0
-      this.nombreOperations = params.operations?.length||0
-      this.nombreImputations = params.imputations?.length||0
-      this.nombreUnitesPhysiques = params.unitePhysiques?.length||0
       this.volumeAE=10
       this.volumeCP=10
     }
