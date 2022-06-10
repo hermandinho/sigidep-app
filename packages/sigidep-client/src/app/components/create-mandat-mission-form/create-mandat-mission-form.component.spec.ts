72659a8811257f8a0331563c52357fc42ecc2c56
Kuete Donald <kuetedonald@gmail.com>
2022-06-10 07:06:55 +0100

    review

--- packages/sigidep-client/src/app/components/create-mandat-mission-form/create-mandat-mission-form.component.spec.ts
+++ packages/sigidep-client/src/app/components/create-mandat-mission-form/create-mandat-mission-form.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CreateMandatMissionFormComponent } from './create-mandat-mission-form.component';
 
 describe('CreateMandatMissionFormComponent', () => {
