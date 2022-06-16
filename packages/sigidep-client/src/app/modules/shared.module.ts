f7db85bc6790e6fd3686841c9e4a4c5f67c20fd7
Kuete Donald <kuetedonald@gmail.com>
2022-06-16 10:24:29 +0100

    review

--- packages/sigidep-client/src/app/modules/shared.module.ts
+++ packages/sigidep-client/src/app/modules/shared.module.ts
@@ -45,6 +45,8 @@ import { PerformFormComponent } from '@components/perform-form/perform-form.comp
 import { CreateMandatFormComponent } from '@components/create-mandat-form/create-mandat-form.component';
 import { PrintEngagementMandatPrimeComponent } from '@components/print-engagement-mandat-prime/print-engagement-mandat-prime.component';
 import { CreateSituationTraitementComponent } from '@components/create-situation-traitement/create-situation-traitement.component';
+import { CreateMandatMissionFormComponent } from '@components/create-mandat-mission-form/create-mandat-mission-form.component';
+import { EngagementMissionFormComponent } from '@components/engagement-mission-form/engagement-mission-form.component';
 
 const declarations = [
   BaseComponent,
